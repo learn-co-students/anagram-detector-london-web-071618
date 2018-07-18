cb5a44f8f09003c7bd2763d28bd5d3d0f7d35bda
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-18 22:09:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  @@anagram = []
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(words)
+    @@anagram = []
+    word_letters = @word.split("")
+    words.each do |word|
+      if word_letters.sort == (word.split("")).sort 
+        @@anagram << word
+      end
+    end
+    @@anagram  
+  end
+  
+  def anagram 
+    @@anagram 
+  end 
+end 
