c47fe320ae03934e46617d18ffc0cd3387ffbc9b
Adham <admuaq@gmail.com>
2018-07-26 08:31:30 +0100

    Done, but last test is too constrictive although my answer is correct

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(sentence_array)
+    #binding.pry
+    array = word.split("")
+
+    arranged = sentence_array.each {|x| x.split(",")}.sort
+    t = arranged.map {|w| w.split("")}
+
+    if t.find {|character| character.sort == array.sort}
+      arranged.select {|x| word.split("").sort == x.split("").sort}
+    else
+      array = []
+    end
+  end
+end
+
+ t = Anagram.new("hello")
+
+
+# binding.pry
